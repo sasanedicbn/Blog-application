c9f1be6dfa0ec7f6450fbf2df41466f7d250b991
Sasa Nedic <svetasmak0@gmail.com>
2025-01-06 20:27:51 +0100

    Connect with mongoose with datebase

--- server/index.js
+++ server/index.js
@@ -6,3 +6,18 @@ import dotenv from "dotenv";
 const app = express();
 app.use(bodyParser.json());
 dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const MONGOURL = process.env.MONGO_URL || 8000;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("DB CONNECTED BAZA");
+    app.listen(PORT, () => {
+      console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR" + err);
+  });
