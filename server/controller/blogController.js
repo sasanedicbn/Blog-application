5dbd14f464192e67f09ab7269c409fadec6a794f
Sasa Nedic <svetasmak0@gmail.com>
2025-01-07 01:45:33 +0100

    Create route for singlePostById

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -27,3 +27,17 @@ export const getAllPosts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getPostById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const singlePost = await modelBlog.findById(id);
+    if (!singlePost) {
+      return res.status(404).json({ message: "Blog posts not found" });
+    }
+
+    res.status(200).json(singlePost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createBlog } from "../controller/blogController.js";
+import {
+  createBlog,
+  getAllPosts,
+  getPostById,
+} from "../controller/blogController.js";
 
 const route = express.Router();
 
 route.post("/blog", createBlog);
+route.get("/blogs", getAllPosts);
+route.get("/blogs/:id", getPostById);
 
 export default route;
