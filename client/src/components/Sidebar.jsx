30c19061bdc3755773f49e09dd4c40edd0b08c24
Sasa Nedic <svetasmak0@gmail.com>
2025-01-09 01:31:00 +0100

    Create Sidebar component

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Categories</h2>
+      <ul>
+        <li>Technology</li>
+        <li>Lifestyle</li>
+        <li>Travel</li>
+        <li>Food</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
