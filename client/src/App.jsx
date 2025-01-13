4139ffeb1e24eaccb45dcb1d1bce0d945de9a3be
Sasa Nedic <svetasmak0@gmail.com>
2025-01-13 01:10:56 +0100

    Refactory two fn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,52 +4,29 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { deletePost, fetchBlogs } from "./components/API/API";
 
 function App() {
   const [blog, setBlog] = useState([]);
 
   useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/api/blogs");
-        const data = await response.json();
-        console.log(data, "risponz");
-        setBlog(data);
-      } catch (error) {
-        console.log("Error", error);
-        toast.error("Failed to load blog posts.");
+    const fetchAllBlogs = async () => {
+      const blogs = await fetchBlogs();
+      if (blogs) {
+        setBlog(blogs);
       }
     };
-    fetchBlogs();
+    fetchAllBlogs();
   }, []);
 
-  const deletePost = async (id) => {
-    try {
-      const response = await fetch(
-        `http://localhost:5000/api/delete/blog/${id}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to delete post");
-      }
-      const result = await response.json();
+  const deletePostHandler = async (id) => {
+    const deletedPost = await deletePost(id);
+    if (deletePost) {
       setBlog((prevBlogs) => prevBlogs.filter((blog) => blog._id !== id));
-      console.log("Blog post deleted:", result);
-      toast.success("Blog post deleted successfully!");
-    } catch (error) {
-      console.error("Error deleting blog post:", error);
-      toast.error("Failed to delete blog post.");
     }
   };
 
   const sendBlogData = async (blogData) => {
-    console.log(blogData, "ovo su dolazni");
     try {
       const response = await fetch("http://localhost:5000/api/blog", {
         method: "POST",
@@ -63,12 +40,11 @@ function App() {
       }
 
       const result = await response.json();
+
       setBlog((prevBlogs) => [...prevBlogs, result]);
-      console.log("Uspješno sam dodao:", result);
       toast.success("Blog post added successfully!");
       return result;
     } catch (error) {
-      console.error("Greška prilikom dodavanja bloga:", error);
       toast.error("Failed to add blog post.");
     }
   };
@@ -86,7 +62,7 @@ function App() {
                 title={post.title}
                 author={post.user}
                 content={post.text}
-                deletePost={deletePost}
+                deletePost={deletePostHandler}
               />
             ))
           ) : (
--- client/src/components/API/API.ts
+++ client/src/components/API/API.ts
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 
  export const getSinglePost = async (id) => {
     try {
@@ -10,5 +11,38 @@
       return result;
     } catch (error) {
       console.error("Error deleting blog post:", error);
+    }
+  };
+
+ export const fetchBlogs = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/blogs");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      toast.error("Failed to load blog posts.");
+    }
+  };
+
+  export const deletePost = async (id) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/delete/blog/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to delete post");
+      }
+      const result = await response.json();
+      return result;
+      toast.success("Blog post deleted successfully!");
+    } catch (error) {
+      toast.error("Failed to delete blog post.");
     }
   };
