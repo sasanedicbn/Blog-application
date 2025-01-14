b9d58180aa6652af29a84a9b0d8e76afcb96a87a
Sasa Nedic <svetasmak0@gmail.com>
2025-01-14 23:56:12 +0100

    Creata LoginForm component

---
+++ client/src/Login/Login.tsx
@@ -0,0 +1,36 @@
+const LoginForm = () => {
+  return (
+    <div className="login-form">
+      <h2 className="login-title">Log In</h2>
+      <form className="form">
+        <label htmlFor="username" className="form-label">
+          Username:
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="form-input"
+          placeholder="Enter your username"
+        />
+
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="form-input"
+          placeholder="Enter your password"
+        />
+
+        <button type="submit" className="login-button">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
