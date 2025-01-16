8bb7d8c9c6bbb949dc174d90cd8f20b700c070ec
Sasa Nedic <svetasmak0@gmail.com>
2025-01-16 22:25:09 +0100

    Create style for login component

--- client/src/Login/Login.tsx
+++ client/src/Login/Login.tsx
@@ -2,35 +2,37 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="login-form">
-      <h2 className="login-title">Log In</h2>
-      <form className="form">
-        <label htmlFor="username" className="form-label">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          className="form-input"
-          placeholder="Enter your username"
-        />
+    <div className="login-backgorund">
+      <div className="login-form">
+        <h2 className="login-title">Log In</h2>
+        <form className="form">
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="form-input"
+            placeholder="Enter your username"
+          />
 
-        <label htmlFor="password" className="form-label">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="form-input"
-          placeholder="Enter your password"
-        />
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-input"
+            placeholder="Enter your password"
+          />
 
-        <button type="submit" className="login-button">
-          Log In
-        </button>
-      </form>
+          <button type="submit" className="login-button">
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
