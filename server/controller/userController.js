7e159e0130326aa2f0dae252fe7a65fdd820d65f
Sasa Nedic <svetasmak0@gmail.com>
2025-01-17 01:57:13 +0100

    Working on registerUser function

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,18 +1,38 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import asyncHandler from "express-async-handler";
+import User from "../model/userModel.js";
 // desc Register new user
 // route POST api/users
 // acess Public
-export const registerUser = (req, res) => {
+export const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
   res.json({ message: "Register User" });
-};
+});
 // desc Authenticate  a user
 // route POST api/users/login
 // acess Public
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
   res.json({ message: "Login User" });
 };
 // desc Get user data
 // route GET api/users/me
 // acess Public
-export const getMe = (req, res) => {
+export const getMe = async (req, res) => {
   res.json({ message: "User data display" });
 };
