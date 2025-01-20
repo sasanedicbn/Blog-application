d607bddff721d3b9600302731c8358691f90c589
Sasa Nedic <svetasmak0@gmail.com>
2025-01-20 15:36:21 +0100

    Create changes

--- server/model/modelBlog.js
+++ server/model/modelBlog.js
@@ -10,7 +10,7 @@ const blogSchema = new mongoose.Schema({
     // required: true,
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: "userModel",
+    ref: "User",
   },
   text: {
     type: String,
