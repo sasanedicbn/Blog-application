da8a409b70d55d698bdb59cc5eaf0a7d7fdb89f6
Sasa Nedic <svetasmak0@gmail.com>
2025-01-25 02:17:35 +0100

    Add logout btn and create fn logouthandler

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import BlogFormModal from "./BlogFormModal";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Header({ sendBlogData }) {
   const [openModal, setOpenModal] = useState(false);
+  const navigation = useNavigate();
 
   const openModalHandler = () => {
     setOpenModal(true);
@@ -10,6 +13,12 @@ function Header({ sendBlogData }) {
 
   const closeModalHandler = () => {
     setOpenModal(false);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    navigation("/login");
+    toast.success("You are logged out!");
   };
 
   const handleNewBlog = async (newBlog) => {
@@ -24,6 +33,7 @@ function Header({ sendBlogData }) {
     <header className="header">
       <h1>My Blog</h1>
       <button onClick={openModalHandler}>NEW BLOG</button>
+      <button onClick={() => logoutHandler()}>Log out</button>
       {openModal && (
         <BlogFormModal
           isOpen={openModal}
